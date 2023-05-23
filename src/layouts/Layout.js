04284a3baee966b663d055f545a741cdbfdededa
Yubraj <iyubrajpoudel@gmail.com>
2023-05-23 15:16:35 +0545

    Resetting scroll value to default on page or location change.

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../components/navbar/Navbar'
 import Footer from './../components/footer/Footer';
+import {useLocation } from "react-router-dom";
 
 const Layout = ({ children }) => {
+    const location = useLocation();
+
+    // Restore the scroll value to default i.e (0,0) when route/location changes Since, page don't get refresh in react
+    useEffect(() => {
+    window.scrollTo(0,0);
+    }
+    , [location])
+
     return (
         <>
             <div className="layout" style={{ overflow: "hidden" }}>
